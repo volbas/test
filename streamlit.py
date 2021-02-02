1a9712ebd6670179354987a16247465690f190f9
Keitaro <keitarou-0416@i.softbank.jp>
2021-02-02 10:56:22 +0900

    1st commit　

---
+++ streamlit.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.title("Streamlit")
+
+
+# git init
+# git remote add origin https://github.com/volbas/test.git
+
+
+# git add .
+# 全てのファイル・ディレクトリを追加
+# # git commit -m "1st commit"　
+# ファイルをコミット
+# # git push origin master
+# ファイルの更新
