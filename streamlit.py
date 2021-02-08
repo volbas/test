69eca7ac18a7427346b3c203e47fa4c46adf74d4
Keitaro <keitarou-0416@i.softbank.jp>
2021-02-08 17:17:00 +0900

    1st commit　

--- streamlit.py
+++ streamlit.py
@@ -2,13 +2,39 @@ import streamlit as st
 st.title("Streamlit")
 
 
+## Github
+# Github>Repositories>New
+# Repository name入力, Public選択
+# Create repository
+
+## Terminal
+# gitの初期化
 # git init
-# git remote add origin https://github.com/volbas/test.git
-
-
-# git add .
+# リポジトリを紐づける
+# git remote add origin https://github.com/volbas/FP.git
+# requirements.txt作成
+# 外部ライブラリを入力
 # 全てのファイル・ディレクトリを追加
-# # git commit -m "1st commit"　
+# git add .
 # ファイルをコミット
-# # git push origin master
+# git commit -m "1st commit"　
 # ファイルの更新
+# git push origin master
+# *** Please tell me who you are.と聞かれたら
+# git config --global user.email ここに自分のアドレス
+# git config --global user.name ここに自分の名前
+
+## Github
+# リロード F5
+# ファイルがアップロードされる
+
+## Streamlit
+# New app
+# 作成したRepositoryを選択肢から選択
+# プログラムを記載したpythonファイル名を入力
+# Deploy!
+# Webアプリ公開完了
+# Your appsからURLをコピーして共有
+
+
+
